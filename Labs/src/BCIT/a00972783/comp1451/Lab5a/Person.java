55614e8fac234115f240e5df0420ae36e24554a3
jlee <jlee@jlee-7050.vivonet.com>
2018-03-28 14:28:58 -0700

    Test

--- Labs/src/BCIT/a00972783/comp1451/Lab5a/Person.java
+++ Labs/src/BCIT/a00972783/comp1451/Lab5a/Person.java
@@ -1,6 +1,83 @@
 package BCIT.a00972783.comp1451.Lab5a;
 
 public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private int birthYear;
+	private boolean isFemale;
+	
+	public Person (String firstName, String lastName, int birthYear, boolean isFemale)
+	{
+		setFirstName(firstName);
+		setLastName(lastName);
+		setBirthYear(birthYear);
+		setFemale(isFemale);
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		if(firstName != null & firstName.length() > 0) 
+		{
+			this.firstName = firstName;
+		}
+		else
+		{
+			System.err.println("Not a valid input for name, try again");
+		}
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		if(lastName != null & lastName.length() > 0) 
+		{
+			this.lastName = lastName;
+		}
+		else
+		{
+			System.err.println("Not a valid input for name, try again");
+		}
+	}
+	
+	public int getBirthYear() {
+		return birthYear;
+	}
+	
+	public void setBirthYear(int birthYear) {
+		if(birthYear > 0) 
+		{
+			this.birthYear = birthYear;
+		}
+		else
+		{
+			System.err.println("Not a valid input for year, try again");
+		}
+	}
+	
+	public boolean isFemale() {
+		return isFemale;
+	}
+	
+	public void setFemale(boolean isFemale) {
+		this.isFemale = isFemale;
+	}
+	
+	public void printDetails() {
+		
+		if(isFemale == true) {
+		System.out.println( firstName + " " + lastName + " is a " + getClass().getSimpleName() + ". She was born in " + birthYear + ". ");
+		}
+		
+		else {
+		System.out.println( firstName + " " + lastName + " is a " + getClass().getSimpleName() + ". He was born in " + birthYear + ". ");
+		}
+	}
+	
 		
 }
---
+++ Labs/src/BCIT/a00972783/comp1451/Lab5a/Tester.java
@@ -0,0 +1,11 @@
+package BCIT.a00972783.comp1451.Lab5a;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Person p = new Person("Jason", "Lee", 1991, false);
+		p.printDetails();
+
+	}
+
+}
